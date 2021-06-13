7fb2f7cb07159a146d642f5d314505924e529d6b
Sujeet <sujeetgupta13hr@gmail.com>
2021-06-13 22:46:16 +0530

    Angular day 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template: '<h1>{{title}}</h1>', this will also work, if we want to load ay html file
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'oneCoversion';
+  @Input() data:string;
+  @Output() params:string;
+
+  showMsg=true;
+  colorName= 'green';
+
+  readMe(){
+    console.log('button clicked')
+  }
 }
