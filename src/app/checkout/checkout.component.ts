2a6e930e9a004f9fcbe7cbc392295c77d7dc1662
Sujeet <sujeetgupta13hr@gmail.com>
2021-06-17 18:57:40 +0530

    Reactive forms Validation

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -10,12 +10,22 @@ export class CheckoutComponent implements OnInit {
 
   checkoutForm: FormGroup; //step #3 give same form group name as in the template
   constructor(private formBuilder: FormBuilder) {
+    /*
     this.checkoutForm = formBuilder.group( //Building the from using formbuilder
     {
     emailAddr: new FormControl(), //in the formbuilder we are craeting a grp of frm elements
     quantity: new FormControl(), //these shud be excatly same as ur form 
     terms: new FormControl()    //we can also have it dynamically, we will learn in next  tuto
     })
+    */
+
+    this.checkoutForm = formBuilder.group( //Building the from using formbuilder
+      {
+      emailAddr: ['', [Validators.required, Validators.email, Validators.minLength(5), Validators.maxLength(10)]], 
+      quantity: ['', Validators.required],  
+      terms:  ['', Validators.requiredTrue]
+      })
+
    }
 
    postData(){
